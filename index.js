05a28b097575491d25620a67ce56502791083c29
Radu Micu <radu.micu@gmail.com>
2019-03-27 14:58:21 +0000

    Fix npm module missing rules 😳

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// VSCode Stylelint extension fails with with Function.Module._resolveFilename error
-// if we do not import using the extension for some reason. Until that is fixed
-// we will import the rules using the `.js` extension, although looks like a Stylelint issue.
-const possibleErrors = require('./rules/possible-errors.js');
-const limitLanguageFeatures = require('./rules/limit-language-features.js');
-const stylisticIssues = require('./rules/stylistic-issues.js');
-const scss = require('./rules/scss.js');
+const possibleErrors = require('./rules/possible-errors');
+const limitLanguageFeatures = require('./rules/limit-language-features');
+const stylisticIssues = require('./rules/stylistic-issues');
+const scss = require('./rules/scss');
 
 module.exports = {
 	plugins: ["stylelint-scss", "stylelint-no-unsupported-browser-features"],
